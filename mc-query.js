3e677959353d2d91c8456aa23a2bd30caada573a
Vinnie <The.CoolV1994@gmail.com>
2026-01-20 22:10:22 -0500

    Update mc-query.js

--- mc-query.js
+++ mc-query.js
@@ -1,1 +1,24 @@
+function queryServer(ip, port, protocol)
+{
+  
+}
 
+function getStatus(data)
+{
+
+}
+
+function getPlayers(data)
+{
+
+}
+
+function getInfo(data)
+{
+
+}
+
+function getPlugins(data)
+{
+
+}
