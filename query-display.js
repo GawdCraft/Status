547ea97e343a0eeb7e489bbf68ded0a047093e6f
Vinnie <The.CoolV1994@gmail.com>
2026-01-20 23:31:38 -0500

    Add elements object for query display

---
+++ query-display.js
@@ -0,0 +1,19 @@
+var elements = {
+
+  "info": "info",
+  "info_title": "info-title",
+  "info_list": "info-list",
+  "info_item": "info-item",
+  "info_none": "info_none",
+
+  "players": "players",
+  "players_title": "players-title",
+  "players_list": "players-list",
+  "players_item": "players-item",
+  "players_none": "players-none",
+
+  "exception": "exception",
+  "exception_message": "",
+  "exception_trace": "",
+  
+};
